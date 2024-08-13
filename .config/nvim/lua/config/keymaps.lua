8850122a057a826fa9d43075c0f7abf61e91ce65
Clark Dwain Luna <37160059+lclarkdwain@users.noreply.github.com>
2024-08-14 06:48:34 +0800

    nvim config fixes default overrides

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,1 +1,13 @@
--- TODO: keymaps
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+keymap.set("n", "x", '"_x')
+
+-- Diagnostic
+keymap.set("n", "<C-j>", function()
+  vim.diagnostic.goto_next()
+end, opts)
